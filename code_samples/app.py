8efc1171d36b8f7274e92ac89bcaaf492b0c02b9
Luke Lee <durdenmisc@gmail.com>
2012-01-15 09:51:44 -0600

    Incorporate simple qwt plot onto qt window

--- code_samples/app.py
+++ code_samples/app.py
@@ -6,18 +6,34 @@ Simple app to demonstrate writing an MVC application for oil industry.
 
 import sys
 from PyQt4 import QtGui
+import PyQt4.Qwt5 as Qwt
+import numpy as np
+
+def qwt():
+    """Show data in qwt plot"""
+
+    plot = Qwt.QwtPlot()
+    plot.setTitle("Oil Production by Year")
+    curve = Qwt.QwtPlotCurve("Barrels (in thousands)")
+
+    x = np.arange(-2*np.pi, 2*np.pi, 0.01)
+    y = np.arange(-2*np.pi, 2*np.pi, 0.01)
+
+    curve.setData(x, y)
+    curve.attach(plot)
+    plot.replot()
+
+    return plot
 
 def main():
     """main"""
 
     app = QtGui.QApplication(sys.argv)
+    window = QtGui.QMainWindow()
 
-    w = QtGui.QWidget()
-    w.resize(250, 150)
-    w.move(300, 300)
-    w.setWindowTitle('Oil Production')
-    w.show()
-    
+    window.setCentralWidget(qwt())
+    window.show()
+
     sys.exit(app.exec_())
 
 if __name__ == "__main__":
